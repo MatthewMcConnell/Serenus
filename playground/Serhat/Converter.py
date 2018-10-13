abff885f316d92cbdfc49cb6c1244778d964219a
Serhat Ordek <2238153o@student.gla.ac.uk>
2018-10-13 15:39:08 +0100

    Added the get_pitch things over for now to test

--- playground/Serhat/Converter.py
+++ playground/Serhat/Converter.py
@@ -3,6 +3,54 @@ import numpy as np
 from numpy import array
 import matplotlib.pyplot as plot
 
+import sys
+from aubio import source, pitch
+from numpy import array, ma
+
+if len(sys.argv) < 2:
+    print("Usage: %s <filename> [samplerate]" % sys.argv[0])
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+downsample = 1
+samplerate = 44100 // downsample
+if len( sys.argv ) > 2: samplerate = int(sys.argv[2])
+
+win_s = 4096 // downsample # fft size
+hop_s = 512  // downsample # hop size
+
+s = source(filename, samplerate, hop_s)
+samplerate = s.samplerate
+
+tolerance = 0.8
+
+pitch_o = pitch("yin", win_s, hop_s, samplerate)
+pitch_o.set_unit("midi")
+pitch_o.set_tolerance(tolerance)
+
+pitches = []
+confidences = []
+
+# total number of frames read
+total_frames = 0
+
+while True:
+    samples, read = s()
+    pitch = pitch_o(samples)[0]
+    confidence = pitch_o.get_confidence()
+    print("%f %f" % (pitch, confidence))
+    pitches += [pitch]
+    confidences += [confidence]
+    total_frames += read
+    if read < hop_s: break
+
+pitches = array(pitches[1:])
+confidences = array(confidences[1:])
+times = [t * hop_s for t in range(len(pitches))]
+cleaned_pitches = ma.masked_where(confidences < tolerance, pitches)
+cleaned_pitches = cleaned_pitches[cleaned_pitches.mask == False] # reliable values
+
 # Get x values of the sine wave
 
 #time = np.arange(0, 10, 0.1);
@@ -54,10 +102,10 @@ def tempoMap(list):
 
     plot.plot(xValues, yValues)
     plot.show()
-    
+
     return numpyY,xValues
 
 a = [x for x in range(0, 100)]
-tempoMap(a);
+tempoMap(cleaned_pitches);
 
 
