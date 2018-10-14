20ae5c19c365465e3734410fdfcbb9746e32e139
Hastai <hastaigr@gmail.com>
2018-10-14 11:34:52 +0100

    made y-position scale with volume

--- playground/Gloria/rain.py
+++ playground/Gloria/rain.py
@@ -2,45 +2,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import get_data as aub
-from scale import scale
+from scale import listscale, rgbscale
 from wavlength import wavlength
 import pyaudio, wave
-import atexit
-import random
-import time
-import math
+import atexit, random, time, math
 import mutagen.wavpack
-# Random seed
-np.random.seed(29680801)
-
 # Audio
 pA = pyaudio.PyAudio()
-wav = wave.open("../../80s.wav")
+wav = wave.open("../../../test.wav")
 stream = pA.open(format =
                 pA.get_format_from_width(wav.getsampwidth()),
                 channels = wav.getnchannels(),
                 rate = wav.getframerate(),
                 output = True)
-chunk = 1250
+chunk = 2048
 data = wav.readframes(chunk)
 
 # Constant declarations
 
 INTERVAL = 10
 DROP_COUNT = 50
-FILELENGTH = (wavlength("../../80s.wav"))
+FILELENGTH = (wavlength("../../../test.wav"))
 
 # Variable to control the pitch up and down
-pitch_list = aub.get_pitch("../../80s.wav")
-tempo_list = aub.get_tempo("../../80s.wav")
+PITCH_LIST = aub.get_pitch("../../../test.wav")
+TEMPO_LIST = aub.get_tempo("../../../test.wav")
+VOLUME = listscale(aub.get_volume("../../../test.wav"))
+print(len(PITCH_LIST))
+print(len(VOLUME))
+print(TEMPO_LIST)
 
-
-pitch_scaled = scale(pitch_list)
-tempo_scaled = scale(tempo_list)
-print(len(tempo_list), len(pitch_list))
-time_position = 1
-
-pitch_time_ratio = len(pitch_list) / FILELENGTH
+## CONSTANTS
+VOLUME_SCALING = 0.8
+PITCH_SCALED = listscale(PITCH_LIST)
+TEMPO_SCALED = listscale(TEMPO_LIST)
+print(len(TEMPO_LIST), len(PITCH_LIST))
+PITCH_TIME_RATIO = len(PITCH_LIST) / FILELENGTH
+VOLUME_TIME_RATIO = len(VOLUME) / FILELENGTH
 
 # Creates a figurue and removes the toolbar.
 plt.rcParams['toolbar'] = 'None'
@@ -65,19 +63,18 @@ rain_drops['growth'] = np.random.uniform(50, 200, DROP_COUNT)
 # Construct the scatter which we will update during animation
 # as the raindrops develop.
 scat = ax.scatter(rain_drops['position'][:, 0], rain_drops['position'][:, 1],
-                  s=rain_drops['size'], lw=0.5, edgecolors='none',
+                  s=20, lw=0.5, edgecolors='none',
                   facecolors=rain_drops['color'])
 T0 = time.time()
-print(pitch_time_ratio)
+print(PITCH_TIME_RATIO)
 
 def update(frame_number):
-
-    time_position = math.floor((time.time() - T0) * pitch_time_ratio)
-    if time_position >= len(pitch_list):
+    time_position = math.floor((time.time() - T0) * PITCH_TIME_RATIO)
+    volume_position = math.floor((time.time() - T0) * VOLUME_TIME_RATIO)
+    if time_position >= len(PITCH_LIST):
         return
     # Get an index which we can use to re-spawn the oldest raindrop.
     current_index = frame_number % DROP_COUNT
-    print(current_index)
 
     # Make all colors more transparent as time progresses.
     rain_drops['color'][:, 3] -= 1.0/len(rain_drops)
@@ -86,21 +83,18 @@ def update(frame_number):
     # Make all circles bigger.
     rain_drops['size'] += rain_drops['growth']
 
-    r = pitch_scaled[time_position-1]
-    g = pitch_scaled[time_position-1]
-    b = pitch_scaled[time_position-1]#pitch_list[int(time_position)]/50.0
-    #g = pitch_list[int(time_position)]/100.0
-    #b = pitch_list[int(time_position)]/100.0
-    # Pick a new position for oldest rain drop, resetting its size,
-    # color and growth factor.
-    #print(r,g, b)
+    r = PITCH_SCALED[time_position-1]
+    g = PITCH_SCALED[time_position-1]
+    b = PITCH_SCALED[time_position-1]
+
+
     rain_drops['position'][current_index, 0] = np.random.uniform(0, 1, 1)
-    rain_drops['position'][current_index, 1] = np.random.uniform(0.4+pitch_list[time_position-1]-pitch_list[time_position], 0.6+pitch_list[time_position-1]-pitch_list[time_position], 1)
+    rain_drops['position'][current_index, 1] = np.random.uniform(0.5-VOLUME[volume_position] * VOLUME_SCALING, 0.5+VOLUME[volume_position] * VOLUME_SCALING, 1)
     rain_drops['size'][current_index] = 5
     rain_drops['color'][current_index] = (r, 0, 0, 1)
     rain_drops['growth'][current_index] = np.random.uniform(50, 200)
 
-    # Update the scatter collection, with the new colors, sizes and positions.
+    # update scatter plot data
     scat.set_facecolors(rain_drops['color'])
     scat.set_sizes(rain_drops['size'])
     scat.set_offsets(rain_drops['position'])
@@ -114,6 +108,6 @@ def update(frame_number):
 # Construct the animation, using the update function as the animation director.
 animation = FuncAnimation(fig, update, interval=INTERVAL)
 fig.patch.set_facecolor((0.8, 0.9, 0.8))
-fig.canvas.manager.full_screen_toggle() # toggle fullscreen mode
-fig.show()
+#fig.canvas.manager.full_screen_toggle() # toggle fullscreen mode
+#fig.show()
 plt.show()
